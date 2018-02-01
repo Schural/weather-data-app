9240c905ced0324e4c277303b2389ec63a27274b
Georg Schneider <georgschneider22@gmail.com>
2018-02-01 16:35:29 +0100

    Changed input to automated .csv file from Spark Cluster

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const yargs = require('yargs');
 const axios = require('axios');
 const geoip = require('geoip-lite');
+const csv = require('fast-csv');
 
 const apiKeys = require('./config.js');
 
-const argv = yargs
+/*const argv = yargs
   .options({
     ipaddress: {
       alias: 'ip',
@@ -20,22 +21,111 @@ const argv = yargs
   .help()
   .alias('help', 'h')
   .argv;
+*/
 
+csv
+ .fromPath("./tmp_exports_merged_orderIP.csv", {headers: true})
+ .on("data", function(data){
+  var inputTimestamp = data.serverTimeUnix;
+  var resultTimestamps = [];
+
+
+  var geo = geoip.lookup(data.ipEnsighten);
+  if (geo) {
+    var lat = geo.ll[0];
+    var lng = geo.ll[1];
+
+    var weatherUrl = `https://api.darksky.net/forecast/${apiKeys.keys.darkskyAPIKey}/${lat},${lng},${inputTimestamp}?exclude=currently,daily,flags,alerts&units=ca`;
+    axios.get(weatherUrl).then((response) => {
+      // Log full response for better understanding the result
+      // console.log(JSON.stringify(response.data, undefined, 2));
+      // console.log('--');
+
+      // push only timestamps from result into array resultTimestamps
+      for (i = 0; i < response.data.hourly.data.length; i++) {
+        resultTimestamps.push(response.data.hourly.data[i].time);
+      }
+
+      // find nearest timestamp
+      smallestDiff = Math.abs(inputTimestamp - resultTimestamps[0]);
+      closestTimestamp = 0;
+
+      for (i = 0; i < resultTimestamps.length; i++ ) {
+          currentDiff = Math.abs(inputTimestamp - resultTimestamps[i]);
+          if (currentDiff < smallestDiff) {
+              smallestDiff = currentDiff;
+              closestTimestamp = i;
+          }
+      }
+
+      var nearestResult = response.data.hourly.data.find( (obj) => {
+        return obj.time = resultTimestamps[closestTimestamp]
+      });
+
+      console.log('Found nearest object in Array');
+      console.log(JSON.stringify(nearestResult, undefined, 2));
+      console.log('Input date: ', new Date(inputTimestamp*1000));
+      console.log('Closest date: ', new Date(resultTimestamps[closestTimestamp]*1000));
+      console.log('--\n')
+    }).catch((e) => {
+      if (e) {
+        console.log(e.message);
+      }
+    });
+
+  } else {
+    console.log('not a valid ip');
+  }
+ })
+ .on("end", function(){
+     //console.log("done");
+ });
+
+ /*
 if (argv.ipaddress && argv.date) {
   console.log(`Return historical weather from ${argv.ipaddress} on ${new Date(argv.date*1000)}`);
   console.log('--');
+
+  var inputTimestamp = argv.date;
+  var resultTimestamps = [];
+
 
   var geo = geoip.lookup(argv.ipaddress);
   if (geo) {
     var lat = geo.ll[0];
     var lng = geo.ll[1];
 
-    var weatherUrl = `https://api.darksky.net/forecast/${apiKeys.keys.darkskyAPIKey}/${lat},${lng},${argv.date}?exclude=currently,minutely,hourly,flags`;
+    var weatherUrl = `https://api.darksky.net/forecast/${apiKeys.keys.darkskyAPIKey}/${lat},${lng},${inputTimestamp}?exclude=currently,daily,flags,alerts&units=ca`;
     axios.get(weatherUrl).then((response) => {
-      //console.log(JSON.stringify(response.data, undefined, 2));
-      console.log(`Summary: ${response.data.daily.data[0].summary}`);
-      var temperatureInCelsius = (((response.data.daily.data[0].temperatureMin + response.data.daily.data[0].temperatureMax) / 2 ) - 32 ) * 5 / 9;
-      console.log(`Average Temperature: ${temperatureInCelsius}`);
+      // Log full response for better understanding the result
+      // console.log(JSON.stringify(response.data, undefined, 2));
+      // console.log('--');
+
+      // push only timestamps from result into array resultTimestamps
+      for (i = 0; i < response.data.hourly.data.length; i++) {
+        resultTimestamps.push(response.data.hourly.data[i].time);
+      }
+
+      // find nearest timestamp
+      smallestDiff = Math.abs(inputTimestamp - resultTimestamps[0]);
+      closestTimestamp = 0;
+
+      for (i = 0; i < resultTimestamps.length; i++ ) {
+          currentDiff = Math.abs(inputTimestamp - resultTimestamps[i]);
+          if (currentDiff < smallestDiff) {
+              smallestDiff = currentDiff;
+              closestTimestamp = i;
+          }
+      }
+
+      var nearestResult = response.data.hourly.data.find( (obj) => {
+        return obj.time = resultTimestamps[closestTimestamp]
+      });
+
+      console.log('Found nearest object in Array');
+      console.log(JSON.stringify(nearestResult, undefined, 2));
+      console.log('Input date: ', new Date(inputTimestamp*1000));
+      console.log('Closest date: ', new Date(resultTimestamps[closestTimestamp]*1000));
     }).catch((e) => {
       if (e) {
         console.log(e.message);
@@ -48,3 +138,5 @@ if (argv.ipaddress && argv.date) {
 } else {
   console.log('IP or Date not specified.');
 }
+
+*/
