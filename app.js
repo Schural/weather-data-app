e1da1f6186b5ac82fd683548b9114ade81d09ab3
Georg Schneider <georgschneider22@gmail.com>
2018-03-01 16:11:32 +0100

    Added queued API requests

--- app.js
+++ app.js
@@ -1,63 +1,154 @@
-const axios = require('axios');
-const geoip = require('geoip-lite');
-const csv = require('fast-csv');
+const csv = require('csvtojson');
+const json2csv = require('json2csv').parse;
+const fs = require('fs');
+
+var request = require("request");
+var RequestQueue = require("limited-request-queue");
 
 const apiKeys = require('./config.js');
+const csvFilePath = 'import_data_without_new_headers.csv';
+const months = [
+  'January', 'February', 'March', 'April', 'May',
+  'June', 'July', 'August', 'September',
+  'October', 'November', 'December'
+];
 
-csv
- .fromPath("./tmp_exports_merged_orderIP.csv", {headers: true})
- .on("data", function(data){
-  var inputTimestamp = data.serverTimeUnix;
-  var resultTimestamps = [];
+var dataArray = [];
+var googleUrls = [];
+var darkskyUrls = [];
+var currentArrayPositionGoogleUrls = 0;
+var currentArrayPositionDarkskyUrls = 0;
 
+function monthNameToNum(monthname) {
+  return month = months.indexOf(monthname);
+}
 
-  var geo = geoip.lookup(data.ipEnsighten);
-  if (geo) {
-    var lat = geo.ll[0];
-    var lng = geo.ll[1];
+csv()
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj) => {
 
-    var weatherUrl = `https://api.darksky.net/forecast/${apiKeys.keys.darkskyAPIKey}/${lat},${lng},${inputTimestamp}?exclude=currently,daily,flags,alerts&units=ca`;
-    axios.get(weatherUrl).then((response) => {
-      // Log full response for better understanding the result
-      // console.log(JSON.stringify(response.data, undefined, 2));
-      // console.log('--');
+    var encodedAddress = encodeURIComponent(jsonObj["GeoSegmentation Cities"] + ", " + jsonObj["GeoSegmentation Countries"]);
+    var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKeys.keys.googleMapsAPIKey}`;
 
-      // push only timestamps from result into array resultTimestamps
-      for (i = 0; i < response.data.hourly.data.length; i++) {
-        resultTimestamps.push(response.data.hourly.data[i].time);
-      }
+    googleUrls.push(geocodeUrl);
+    dataArray.push(jsonObj);
+  })
+  .on('done', (error) => {
+    googleUrls.forEach(queueGoogleRequests.enqueue, queueGoogleRequests);
+  });
 
-      // find nearest timestamp
-      smallestDiff = Math.abs(inputTimestamp - resultTimestamps[0]);
-      closestTimestamp = 0;
+var queueGoogleRequests = new RequestQueue(null, {
+  item: function (input, done) {
+    request(input.url, function (error, response, body) {
 
-      for (i = 0; i < resultTimestamps.length; i++ ) {
-          currentDiff = Math.abs(inputTimestamp - resultTimestamps[i]);
-          if (currentDiff < smallestDiff) {
-              smallestDiff = currentDiff;
-              closestTimestamp = i;
-          }
-      }
+      body = JSON.parse(body);
 
-      var nearestResult = response.data.hourly.data.find( (obj) => {
-        return obj.time = resultTimestamps[closestTimestamp]
-      });
+      if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find address, pushing localhost to darkskyUrls Array, to keep same size');
+        darkskyUrls.push('http://127.0.0.1');
+        currentArrayPositionGoogleUrls++;
+        done();
+      } else {
+        var lat = body.results[0].geometry.location.lat;
+        var lng = body.results[0].geometry.location.lng;
 
-      console.log('Found nearest object in Array');
-      console.log(JSON.stringify(nearestResult, undefined, 2));
-      console.log('Input date: ', new Date(inputTimestamp*1000));
-      console.log('Closest date: ', new Date(resultTimestamps[closestTimestamp]*1000));
-      console.log('--\n')
-    }).catch((e) => {
-      if (e) {
-        console.log(e.message);
+        var adobeDate = dataArray[currentArrayPositionGoogleUrls].Hour;
+        var timezoneOffset = dataArray[currentArrayPositionGoogleUrls]["Time Zones"] - 1;
+
+        var seperatedAdobeDate = adobeDate.split(',');
+
+        var adobeMonth = monthNameToNum(seperatedAdobeDate[0].split(' ')[0].trim());
+        var adobeDay = seperatedAdobeDate[0].split(' ')[1].trim();
+        var adobeYear = seperatedAdobeDate[1].trim();
+        var adobeHour = seperatedAdobeDate[2].split(' ')[2].trim();
+
+        var adobeTimeDateObject = new Date();
+
+        adobeTimeDateObject.setUTCDate(Number(adobeDay));
+        adobeTimeDateObject.setUTCMonth(Number(adobeMonth));
+        adobeTimeDateObject.setUTCFullYear(Number(adobeYear));
+        adobeTimeDateObject.setUTCHours(Number(adobeHour) - timezoneOffset);
+        adobeTimeDateObject.setUTCMinutes(0);
+        adobeTimeDateObject.setUTCSeconds(0);
+        adobeTimeDateObject.setUTCMilliseconds(0);
+
+        unixTimestamp = adobeTimeDateObject.getTime() / 1000;
+
+        var weatherUrl = `https://api.darksky.net/forecast/${apiKeys.keys.darkskyAPIKey}/${lat},${lng},${unixTimestamp}?exclude=hourly,daily,flags,alerts&units=ca`;
+        darkskyUrls.push(weatherUrl);
+        currentArrayPositionGoogleUrls++;
+        done();
       }
     });
-
-  } else {
-    console.log('not a valid ip');
+  },
+  end: function () {
+    console.log("Google Request Queue completed!");
+    darkskyUrls.forEach(queueDarkskyRequests.enqueue, queueDarkskyRequests);
   }
- })
- .on("end", function(){
-     //console.log("done");
- });
+});
+
+var queueDarkskyRequests = new RequestQueue(null, {
+  item: function (input, done) {
+    request(input.url, function (error, response, body) {
+
+      if (input.url == 'http://127.0.0.1') {
+        console.log('no valid request was possible, due to missing url');
+        dataArray[currentArrayPositionDarkskyUrls].latitude = null;
+        dataArray[currentArrayPositionDarkskyUrls].longitude = null;
+        dataArray[currentArrayPositionDarkskyUrls].unixTimestamp = null;
+        dataArray[currentArrayPositionDarkskyUrls].apparentTemperature = null;
+        dataArray[currentArrayPositionDarkskyUrls].cloudCover = null;
+        dataArray[currentArrayPositionDarkskyUrls].dewPoint = null;
+        dataArray[currentArrayPositionDarkskyUrls].humidity = null;
+        dataArray[currentArrayPositionDarkskyUrls].icon = null;
+        dataArray[currentArrayPositionDarkskyUrls].cloudCover = null;
+        dataArray[currentArrayPositionDarkskyUrls].precipIntensity = null;
+        dataArray[currentArrayPositionDarkskyUrls].precipProbability = null;
+        dataArray[currentArrayPositionDarkskyUrls].precipType = null;
+        dataArray[currentArrayPositionDarkskyUrls].pressure = null;
+        dataArray[currentArrayPositionDarkskyUrls].summary = null;
+        dataArray[currentArrayPositionDarkskyUrls].temperature = null;
+        dataArray[currentArrayPositionDarkskyUrls].visibility = null;
+        dataArray[currentArrayPositionDarkskyUrls].windBearing = null;
+        dataArray[currentArrayPositionDarkskyUrls].windSpeed = null;
+        currentArrayPositionDarkskyUrls++;
+        done();
+      } else {
+
+        body = JSON.parse(body);
+
+        dataArray[currentArrayPositionDarkskyUrls].latitude = body.latitude || null;
+        dataArray[currentArrayPositionDarkskyUrls].longitude = body.longitude || null;
+        dataArray[currentArrayPositionDarkskyUrls].unixTimestamp = body.currently.time || null;
+        dataArray[currentArrayPositionDarkskyUrls].apparentTemperature = body.currently.apparentTemperature || null;
+        dataArray[currentArrayPositionDarkskyUrls].cloudCover = body.currently.cloudCover || null;
+        dataArray[currentArrayPositionDarkskyUrls].dewPoint = body.currently.dewPoint || null;
+        dataArray[currentArrayPositionDarkskyUrls].humidity = body.currently.humidity || null;
+        dataArray[currentArrayPositionDarkskyUrls].icon = body.currently.icon || null;
+        dataArray[currentArrayPositionDarkskyUrls].cloudCover = body.currently.cloudCover || null;
+        dataArray[currentArrayPositionDarkskyUrls].precipIntensity = body.currently.precipIntensity || null;
+        dataArray[currentArrayPositionDarkskyUrls].precipProbability = body.currently.precipProbability || null;
+        dataArray[currentArrayPositionDarkskyUrls].precipType = body.currently.precipType || null;
+        dataArray[currentArrayPositionDarkskyUrls].pressure = body.currently.pressure || null;
+        dataArray[currentArrayPositionDarkskyUrls].summary = body.currently.summary || null;
+        dataArray[currentArrayPositionDarkskyUrls].temperature = body.currently.temperature || null;
+        dataArray[currentArrayPositionDarkskyUrls].visibility = body.currently.visibility || null;
+        dataArray[currentArrayPositionDarkskyUrls].windBearing = body.currently.windBearing || null;
+        dataArray[currentArrayPositionDarkskyUrls].windSpeed = body.currently.windSpeed || null;
+
+        currentArrayPositionDarkskyUrls++;
+        done();
+      }
+
+    });
+  },
+  end: function () {
+    console.log("Darksky Request Queue completed!");
+    try {
+      const csv = json2csv(dataArray);
+      fs.writeFileSync('import_data_without_new_headers2.csv', csv);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
